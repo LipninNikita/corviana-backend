2fb16db3c46e6d51ce70f5eba2fcbfa1a7fab72a
Никита Липнин <lipnin.ne@it-rkomi.ru>
2023-12-09 12:00:28 +0300

    isfree migrations questions

---
+++ src/Services/Question/Question.API/Data/Migrations/20231209090012_isfree.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Question.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class isfree : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsFree",
+                table: "Questions",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.CreateTable(
+                name: "UserQuestionTransactions",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    UserId = table.Column<string>(type: "text", nullable: false),
+                    QuestionId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserQuestionTransactions", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserQuestionTransactions");
+
+            migrationBuilder.DropColumn(
+                name: "IsFree",
+                table: "Questions");
+        }
+    }
+}
