8df451430a05af789f531e11afcbbb0bc180e244
Никита Липнин <lipnin.ne@it-rkomi.ru>
2023-12-09 19:54:58 +0300

    string to int questionapi

--- src/Services/Question/Question.API/DTO/AnswerInput.cs
+++ src/Services/Question/Question.API/DTO/AnswerInput.cs
@@ -2,7 +2,7 @@
 {
     public class AnswerInput
     {
-        public string QuestionId { get; set; }
+        public int QuestionId { get; set; }
         public bool IsSuccess { get; set; }
     }
 }
